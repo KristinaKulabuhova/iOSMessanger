d4a2c62c175708fd2edfd74c6c789bc09c5b2b6a
Kristina Kulabukhova <kulabukhova.ka@phystech.edu>
2022-02-27 22:36:34 +0300

    Initial Commit

---
+++ Messager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messager
+//
+//  Created by Kristina on 27.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
